ce365cc912d1f877bd310d517e3059fd717001a7
akshaymurari <akshaymurari184@gmail.com>
2020-12-15 23:12:08 +0530

    added css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 const App = () => {
     return (
         <>
-          <h1>hii..</h1>
+          <div className="container-sm">
+            <div className="row m-sm-5">
+              <div className="col-md-6 m-auto cardd">
+                <form>
+                  <h1 className="text-center text-black-50">Weather App</h1>
+                  <input type="text" className="form-control"/>
+                </form>
+              </div>
+            </div>
+          </div>
         </>
     )
 }

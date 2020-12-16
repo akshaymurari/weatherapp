2eb704ff11181aceeeb34c648becd0c6e02dc2f1
akshaymurari1 <suhasvodela123@gmail.com>
2020-12-16 12:54:56 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
-
+import Card from './Card';
 const App = () => {
     return (
         <>
-          <div className="container-sm">
-            <div className="row m-sm-5">
-              <div className="col-md-6 m-auto cardd">
-                <form>
-                  <h1 className="text-center text-black-50">Weather App</h1>
-                  <input type="text" className="form-control"/>
-                </form>
-              </div>
-            </div>
-          </div>
+          <Card />
         </>
     )
 }
